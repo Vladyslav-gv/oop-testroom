ca7f2305b17f0a025cc6667503a22d64058e261e
vlad <gitolvalet@gmail.com>
2026-04-06 09:13:58 +0300

    availability virtual destructor

--- rent_auto/class_vehicle.h
+++ rent_auto/class_vehicle.h
@@ -10,13 +10,32 @@ class Vehicle {
 protected:
     string brand;
     double priceday;
+
 public:
     Vehicle(string b = "Unknown", double p = 0) : brand(b), priceday(p) {
-        cout << "Constructor Vehicle " << endl;
+        cout << "Constructor Vehicle" << endl;
     }
+
     virtual ~Vehicle() {
         cout << "Destructor Vehicle" << endl;
     }
+
+    // Virtual functions for polymorphism
+    virtual void showInfo() const {
+        cout << "Brand: " << brand << endl << " Price per day: " << priceday << "$";
+    }
+
+    virtual string getDescribe() const {
+        return "Generic Vehicle";
+    }
+
+    // Pure virtual function - makes this class abstract
+    virtual double tCost(int days) const = 0;
+
+    virtual void performMain() const {}
+
+    string getBrand() const { return brand; }
+    double getPriceday() const { return priceday; }
 };
 
 #endif
